9485ca287a3a09f3654abb48c639486032fba9a7
Alex Kniazeiv <knyazev22062010@gmail.com>
2026-02-09 20:48:31 -0500

    Modified front end header

--- components/client/header.tsx
+++ components/client/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return(
         
         <header className="sticky top-0 flex bg-purple-900/30 z-100 items-center ">
-           <p className="text-4xl py-4 mx-20">DevCom </p>
+           <p className="text-4xl py-4 mx-20">DevEvents </p>
            <Link href={'/about'} className="absolute right-60">About</Link>
            <Link href={'/about'} className="absolute right-25">My account</Link>
         </header>
